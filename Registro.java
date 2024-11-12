e2ef5c8e6e8f4bfa74454190b8af6ca9dc3c8465
Sergio Tan <stan-2021131@kinal.edu.gt>
2024-11-12 17:37:27 -0600

    Se agregó el gitIgnore, getters y setters de registro

--- Registro.java
+++ Registro.java
@@ -4,4 +4,24 @@ import java.util.Date;
 public class Registro {
     String cambio;
     Date fechaCambio;
+
+    public Registro() {
+    }
+
+    public Registro(String cambio, Date fechaCambio) {
+        this.cambio = cambio;
+        this.fechaCambio = fechaCambio;
+    }
+    public Date getFechaCambio() {
+        return fechaCambio;
+    }
+    public String getCambio() {
+        return cambio;
+    }
+    public void setFechaCambio(Date fechaCambio) {
+        this.fechaCambio = fechaCambio;
+    }
+    public void setCambio(String cambio) {
+        this.cambio = cambio;
+    }
 }
